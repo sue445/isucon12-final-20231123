5808d7e39f2928238a1c99c4bb7f4cc5211e6b56
sue445 <sue445@sue445.net>
2023-11-23 17:59:40 +0900

    GET /user/:user_id/present/index/:n : Add index to user_presents

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -2,6 +2,8 @@ ALTER TABLE user_present_all_received_history ADD INDEX index_user_id_and_presen
 
 ALTER TABLE present_all_masters ADD INDEX index_registered_start_at_and_registered_end_at (registered_start_at, registered_end_at);
 
+ALTER TABLE user_presents ADD INDEX index_user_id_and_deleted_at_and_created_at (user_id, deleted_at, created_at);
+
 /*
 ALTER TABLE login_bonus_masters ADD INDEX index_start_at (start_at);
 
